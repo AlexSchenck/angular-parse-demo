b13b9d9b1b41db720edb499936977365570a8ffe
Alex <AlexNSchenck@gmail.com>
2014-11-13 09:23:04 -0800

    classwork 11/13/14

--- js/github-app.js
+++ js/github-app.js
@@ -4,16 +4,25 @@
 
     To get all your repos on GitHub, GET this URL:
         https://api.github.com/users/your-github-username/repos
+
+        https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
  */
 
 "use strict";
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
-        $scope.userName = 'drstearns';
+    .controller('GitHubController', function($scope, $http) {
+        $scope.userName = 'AlexSchenck';
         $scope.getRepos = function() {
 
-            //add code here
-
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    console.log(angular.toJson(data, true));
+                    $scope.repos = data;
+                })
+                .error(function(err) {
+                    // err will contain error information
+                    console.log(err);
+                });
         };
     });
